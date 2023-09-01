0e273c5b467bfe4ed8f8f26c7595bfaea3280f61
Vitor Hugo <vto.hugo67@gmail.com>
2023-08-31 21:43:30 -0300

    DPT-003 <feat> (First Login Page)

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,24 +1,25 @@
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const config = require('../../config/config'); // Importe suas configurações de JWT
+const config = require('../../config/config');
 const UserModel = require('../models/userModel');
 
 exports.register = async (req, res) => {
   try {
     const { email, password } = req.body;
-
     // Verifique se o usuário já existe
     const existingUser = await UserModel.findOne({ where: { email } });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
-
+    // Obtenha o número de rounds do .env
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
+    // Criptografe a senha usando o salt
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     // Crie um novo usuário
-    const newUser = await UserModel.create({ email, password });
-
+    const newUser = await UserModel.create({ email, password: hashedPassword });
     // Gere o token JWT
     const payload = { user: { id: newUser.id } };
     const token = jwt.sign(payload, config.development.jwtSecret, { expiresIn: '1h' });
-
     res.json({ token });
   } catch (error) {
     console.error(error);
@@ -29,18 +30,19 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
-
-    // Faça a autenticação do usuário (exemplo simples)
-    const user = await UserModel.findOne({ email });
-
-    if (!user || user.password !== password) {
+    // Verifique se o usuário existe
+    const user = await UserModel.findOne({ where: { email } });
+    if (!user) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
-
+    // Verifique a senha criptografada
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
     // Gere o token JWT
     const payload = { user: { id: user.id } };
-    const token = jwt.sign(payload, config.jwtSecret, { expiresIn: '1h' });
-
+    const token = jwt.sign(payload, config.development.jwtSecret, { expiresIn: '1h' });
     res.json({ token });
   } catch (error) {
     console.error(error);
--- config/config.js
+++ config/config.js
@@ -2,11 +2,12 @@ require('dotenv').config(); // Carregar variáveis de ambiente do .env
 
 module.exports = {
   development: {
+    dialect: 'mysql',
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
     jwtSecret: process.env.JWT_SECRET,
   },
   test: {
